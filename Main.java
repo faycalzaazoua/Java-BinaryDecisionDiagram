91b76a072391c686ed9fa425367970a6bd45b218
Clem0908 <clementlyonnet@hotmail.fr>
2021-04-29 16:17:08 +0200

    update

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		Sauvegarde s = new Sauvegarde();
+		Sauvegarde s = new Sauvegarde(null, "test", "creation");
+		s.supprimerSauvegarde("test");
 	}
 }
--- serveur/sauvegarde/Sauvegarde.java
+++ serveur/sauvegarde/Sauvegarde.java
@@ -2,6 +2,7 @@ package serveur.sauvegarde;
 
 import serveur.construction.DDB;
 
+import java.io.IOException;
 import java.lang.NullPointerException;
 import java.io.File;
 
@@ -20,15 +21,21 @@ public class Sauvegarde
 		this.nomProjet = nomProjet;
 		this.etatDDB = etatDDB;
 
-		String chemin = "./" + this.nomProjet + "_save.json";
+		/* Initialisation du nom de fichier final. */
+		String chemin = this.nomProjet + "_save.json";
 
 		try
 		{
-			File saveFile = new File(chemin);
+			/* Créer un fichier de sauvegarde dans le répertoire 'saves'. */
+			File saveFile = new File("saves",chemin);
+			boolean ret = saveFile.createNewFile();
 
-		}catch(NullPointerException cheminNull)
+			if(!(ret))
+				System.out.println("Fichier déjà existant.");
+
+		}catch(NullPointerException | IOException cheminNull)
 		{
-			System.out.println("Erreur, nom de fichier de sauvegarde nul.");
+			System.out.println("Erreur, nom de fichier de sauvegarde nul ou déplacement vers un dossier inexistant.");
 		}
 		//write into json the DDB state
 
@@ -37,5 +44,20 @@ public class Sauvegarde
 	public Sauvegarde(String nomProjet)
 	{
 		this.nomProjet = nomProjet;
+	}
+
+	//Pb : supprime dans tous les cas :/
+	public void supprimerSauvegarde(String nomProjet) {
+		if (nomProjet == null)
+		{
+			System.out.println("Erreur, nom de projet à supprimer vide.");
+			return;
+		}
+
+		String chemin = this.nomProjet + "_save.json";
+		File fileToSuppress = new File("saves", chemin);
+		boolean ret = fileToSuppress.delete();
+			if(!(ret))
+				System.out.println("Fichier à supprimer non trouvé.");
 	}
 }
